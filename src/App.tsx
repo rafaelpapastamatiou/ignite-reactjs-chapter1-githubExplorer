74293a61f941fda9a2ecdd749d09cc2774f7d003
rafaelpapastamatiou <rafaelpapas1412@gmail.com>
2021-03-31 02:00:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.scss';
+
+import { RepositoryList } from './components/RepositoryList';
+
+export function App() {
+  return (
+    <>
+      <RepositoryList/>
+    </>
+  )
+}
